faaee4a3de13ff1260ae990c6ed03bcffdf77eaa
Samuel Lee <samuel.lee@servian.com.au>
2018-06-20 08:35:55 +1000

    added changes for sqlite integration

--- flask-restful-sql-app/security.py
+++ flask-restful-sql-app/security.py
@@ -1,20 +1,13 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1,'bob','asdf')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 def authenticate(username, password):
     #get is useful as it can return none if there is no username instead of using item['chair'] for getting an object
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     #safe way to compare strings from different ascii, unicode etc
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
