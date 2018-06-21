c5316e59cb38e73868482fb2d8574773fd9e29d3
Lee <Samuel.Lee@team.telstra.com>
2018-06-21 16:33:12 +1000

    finishing off put endpoint

--- flask-restful-sql-app/item.py
+++ flask-restful-sql-app/item.py
@@ -11,14 +11,7 @@ class Item(Resource):
         required=True,
         help="this field cannot be left blank"
     )
-
-    @jwt_required()
-    def get(self, name):
-        item = self.find_by_name(name)
-        if item:
-            return item
-        return {'message': 'Item not found'}, 404
-
+    
     @classmethod
     def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
@@ -30,15 +23,9 @@ class Item(Resource):
         connection.close()
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
-
-    def post(self, name):
-        if self.find_by_name(name):
-            return {'message': "an item with name '{}' already exists".format(name)}, 400 #400 is bad request
-        
-        data = Item.parser.parse_args()
-        
-        item = {'name':name, 'price': data['price']}
-        
+    
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -47,30 +34,85 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
+    
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
 
-        return item, 201 
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'],item['name']))
 
+        connection.commit()
+        connection.close()
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+   
+
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': "an item with name '{}' already exists".format(name)}, 400 #400 is bad request
+        
+        data = Item.parser.parse_args()
+        item = {'name':name, 'price': data['price']}
+
+        try:
+            self.insert(item)
+        except Exception:
+            return {"message": "An error occurred inserting the item: {0}".format(Exception)}, 500 #internal server error
+        
+        return item, 201
+
+    @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
+        if self.find_by_name(name):
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
 
+            query = "DELETE FROM items WHERE name=?"
+            cursor.execute(query, (name,))
+
+            connection.commit()
+            connection.close()
+
+            return {'message': 'Item deleted'}
+        return {'message': "no item with name: '{}' exists in database".format(name)}, 404 
+    
+    @jwt_required()
     def put(self, name):
-        #method to update item price or name etc
-        #this method checks if there is already an existing item there
-        #if not add this item if there is then just update its content
-
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name']==name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {"name": name, "price": data['price']}
+
         if item is None: 
-            item={'name': name, 'price': data['price']}
-            items.append(item)
-        else: 
-            #python dictionaries come with update function
-            item.update(data)
-        return item
+            try:
+                self.insert(updated_item)
+            except Exception:
+                return {"message": "An error occurred updating the item: {0}".format(Exception)}, 500 #internal server error
+        else:
+            try:
+                self.update(updated_item)
+            except Exception:
+                return {"message": "An error occurred updating the item: {0}".format(Exception)}, 500 #internal server error 
+        return updated_item
 
 class ItemList(Resource): 
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items=[]
+        for row in result:
+            items.append({'name':row[0], 'price': row[1]}) 
+
+        connection.close()
         return {'items': items}
